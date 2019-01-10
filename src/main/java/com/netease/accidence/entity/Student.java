c7be0bd2109abbe3daf70f6039a3792f4e3986ab
dzh1104 <zhding1104@163.com>
2019-01-10 14:43:05 +0800

    yml配置、多个配置文件、多环境配置文件

--- src/main/java/com/netease/accidence/entity/Student.java
+++ src/main/java/com/netease/accidence/entity/Student.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
