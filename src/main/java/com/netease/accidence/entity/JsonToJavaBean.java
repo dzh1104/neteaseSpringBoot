789960c0f1cd0e58cace5879de543b00e42bedea
dzh1104 <zhding1104@163.com>
2019-01-10 15:37:13 +0800

    添加GsonFormat、RestfulToolkit idea插件，看看效果

---
+++ src/main/java/com/netease/accidence/entity/JsonToJavaBean.java
@@ -0,0 +1,47 @@
+package com.netease.accidence.entity;
+
+public class JsonToJavaBean {
+
+/**
+ * name : jack
+ * age : 19
+ * stuNo : 9
+ */
+
+private String name;
+
+private int age;
+
+private int stuNo;
+
+public String getName() {
+    
+    return name;
+}
+
+public void setName(String name) {
+    
+    this.name = name;
+}
+
+public int getAge() {
+    
+    return age;
+}
+
+public void setAge(int age) {
+    
+    this.age = age;
+}
+
+public int getStuNo() {
+    
+    return stuNo;
+}
+
+public void setStuNo(int stuNo) {
+    
+    this.stuNo = stuNo;
+}
+
+}
